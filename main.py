fb036532d333aca6694f0266a8683f6ca5b84f7a
Teju Gangisetty <tgangise@redhat.com>
2025-07-08 13:55:21 -0400

    langchain framework chatbot

---
+++ main.py
@@ -0,0 +1,58 @@
+from langchain import hub
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langgraph.graph import START, StateGraph
+from typing_extensions import List, TypedDict
+
+# load and chunk contents of the PDF
+loader = PyPDFLoader("data/pod_scenarios.pdf")
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+# embed and store in vector database
+embedding_model = HuggingFaceEmbeddings(model_name="Qwen/Qwen3-Embedding-0.6B")
+vector_store = Chroma.from_documents(documents=all_splits, embedding=embedding_model)
+
+# Define prompt for question-answering
+# N.B. for non-US LangSmith endpoints, you may need to specify
+# api_url="https://api.smith.langchain.com" in hub.pull.
+prompt = hub.pull("rlm/rag-prompt")
+
+# load LLM (chat model)
+llm = ChatOpenAI(model="gpt-4o", temperature=0, openai_api_key= "openAI-api-key" )
+
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: List[Document]
+    answer: str
+
+# Define application steps
+def retrieve(state: State):
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+def generate(state: State):
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+# Compile the graph
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+# run in a loop
+while True:
+    q = input("Ask a question (or type 'exit'): ")
+    if q.lower() in ["exit", "quit"]:
+        break
+    result = graph.invoke({"question": q})
+    print("\nAnswer:", result["answer"])
