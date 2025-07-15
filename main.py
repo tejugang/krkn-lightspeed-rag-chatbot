b57aca7e437f9f87c29f4f43127ce0ce693ce0a5
Teju Gangisetty <tgangise@redhat.com>
2025-07-15 18:02:30 -0400

    added rag_pipeline for llama 2.7 model

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ import time
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from langchain_community.llms import Ollama
-from granite_rag_pipeline import load_granite_rag_pipline
-from llama31_rag_pipeline import load_llama31_rag_pipeline
+from rag_pipelines.granite_rag_pipeline import load_granite_rag_pipline
+from rag_pipelines.llama31_rag_pipeline import load_llama31_rag_pipeline
+from rag_pipelines.llama27_rag_pipeline import load_llama27_rag_pipeline
 
-# COMMENT OUT THE CODE FOR THE MODEL THAT YOU ARE NOT USING BEFORE RUNNING
+
+# UNCOMMENT THE CODE FOR THE MODEL THAT YOU ARE NOT USING BEFORE RUNNING
 
 #START OF GRANITE MODEL LOGIC
 #granite
@@ -57,3 +59,24 @@ while True:
 
     print("\nAnswer:", result["answer"])
 #END OF LLAMA 3.1 MODEL LOGIC
+
+
+#START OF LLAMA 2.7 MODEL LOGIC
+#llama 2.7
+graph = load_llama27_rag_pipeline()
+
+# run in a loop
+while True:
+    q = input("Ask a question (or type 'exit'): ")
+    if q.lower() in ["exit", "quit"]:
+        break
+    start_time = time.time()
+    result = graph.invoke({"question": q})
+    end_time = time.time()
+    duration = end_time - start_time
+    
+    print("Time taken:", round(duration, 2), "seconds")
+
+
+    print("\nAnswer:", result["answer"])
+#END OF LLAMA 2.7 MODEL LOGIC
